29ca5db5a7879cb16b26795e1c261ae80c41b809
Shantanu Singh <shsingh@broadinstitute.org>
2017-01-13 15:48:01 -0500

    Update output.tf

--- security/output.tf
+++ security/output.tf
@@ -6,6 +6,10 @@ output "security_group_ssh_http_id" {
   value = "${aws_security_group.ssh_http.id}"
 }
 
+output "security_group_ssh_http_anywhere_id" {
+  value = "${aws_security_group.ssh_http_anywhere.id}"
+}
+
 output "security_group_nfs_id" {
   value = "${aws_security_group.nfs.id}"
 }
